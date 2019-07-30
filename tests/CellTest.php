59729709f4ae4e4633636a762eade4d31fe0f493
Remy Bos <i231896@fhict.nl>
2019-07-30 18:39:54 +0200

    Add extra cases for testing cell construction

--- tests/CellTest.php
+++ tests/CellTest.php
@@ -8,15 +8,26 @@ class CellTest extends TestCase
     /**
      * @dataProvider constructionProvider
      */
-    public function testConstruction(int $row, int $column, $expected) 
+    public function testConstruction(int $row, int $column, $expected = null) 
     {
-        $this->expectException($expected);
-        new class($row, $column) extends Cell {
-            public function __construct(int $row, int $column) 
-            {
-                parent::__construct($row, $column, 0);
-            }
-        };
+        if ($row < 0 || $column < 0) {
+            $this->expectException($expected);
+            new class($row, $column) extends Cell {
+                public function __construct(int $row, int $column) 
+                {
+                    parent::__construct($row, $column, 0);
+                }
+            };
+        } else {
+            $cell = new class($row, $column) extends Cell {
+                public function __construct(int $row, int $column) 
+                {
+                    parent::__construct($row, $column, 0);
+                }
+            };
+            $this->assertSame($row, $cell->getRow());
+            $this->assertSame($column, $cell->getColumn());
+        }
     }
 
     public function constructionProvider()
@@ -25,6 +36,10 @@ class CellTest extends TestCase
             [-1, 0, InvalidIndexException::class],
             [0, -1, InvalidIndexException::class],
             [-1, -1, InvalidIndexException::class],
+            [0, 0],
+            [rand(0, 10000), rand(0, 10000)],
+            [0, rand(0, 10000)],
+            [rand(0, 10000), 0],
         ];
     }
 }
